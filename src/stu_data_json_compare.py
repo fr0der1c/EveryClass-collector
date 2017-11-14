147d568e19931bf7d215cfdcc1c6bb516cd80458
Frederic <frederic.t.chan@gmail.com>
2017-11-14 09:15:27 +0800

    json compare change into a function

--- src/stu_data_json_compare.py
+++ src/stu_data_json_compare.py
@@ -1,17 +1,23 @@
 import json
 import settings
 
-with open("stu_data_1029.json") as old_file:
-    with open("stu_data_1112.json") as new_file:
-        old = json.load(old_file)
-        new = json.load(new_file)
-        print("Old length:%d" % len(old))
-        print("New length:%d" % len(new))
-        print("In old but not in new:")
-        for each in old:
-            if each not in new:
-                print(each)
-        print("In new but not in old")
-        for each in new:
-            if each not in old:
-                print(each)
+
+def compare_json(old, new):
+    with open(old) as old_file:
+        with open(new) as new_file:
+            old = json.load(old_file)
+            new = json.load(new_file)
+            print("Old length:%d" % len(old))
+            print("New length:%d" % len(new))
+            print("In old but not in new:")
+            for each in old:
+                if each not in new:
+                    print(each)
+            print("In new but not in old")
+            for each in new:
+                if each not in old:
+                    print(each)
+
+
+if __name__ == "__main__":
+    compare_json(old="stu_data_1029.json", new="stu_data_1112.json")
